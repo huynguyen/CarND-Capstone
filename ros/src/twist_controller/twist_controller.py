8b554980f4d34a75813926f674584e814ff31e16
Huy Nguyen <weehuy@gmail.com>
2019-03-18 06:27:28 +0000

    Change braking force to 700Nm according to lesson 8.

--- ros/src/twist_controller/twist_controller.py
+++ ros/src/twist_controller/twist_controller.py
@@ -14,21 +14,25 @@ class Controller(object):
 
         # TODO: Implement
         self.yaw_controller = YawController(wheel_base, steer_ratio, 0.1, max_lat_accel, max_steer_angle)
+
         kp = 0.3
         ki = 0.1
         kd = 0.0
         mn = 0.0
         mx = 0.2
         self.throttle_controller = PID(kp, ki, kd, mn, mx)
+
         tau = 0.5
         ts = 0.02
         self.vel_lpf = LowPassFilter(tau, ts)
+
         self.vehicle_mass = vehicle_mass
         self.fuel_capacity = fuel_capacity
         self.brake_deadband = brake_deadband
         self.decel_limit = decel_limit
         self.accel_limit = accel_limit
         self.wheel_radius = wheel_radius
+
         self.last_time = rospy.get_time()
 
     def control(self, current_vel, dbw_enabled, linear_vel, angular_vel):
@@ -41,8 +45,10 @@ class Controller(object):
         current_vel = self.vel_lpf.filt(current_vel)
 
         steering = self.yaw_controller.get_steering(linear_vel, angular_vel, current_vel)
+        
         vel_error = linear_vel - current_vel
         self.last_vel = current_vel
+
         current_time = rospy.get_time()
         sample_time = current_time - self.last_time
         self.last_time = current_time
@@ -52,7 +58,7 @@ class Controller(object):
 
         if linear_vel == 0. and current_vel < 0.1:
             throttle = 0
-            brake = 400
+            brake = 700
         elif throttle < .1 and vel_error < 0:
             throttle = 0
             decel = max(vel_error, self.decel_limit)
